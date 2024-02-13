62aa5c4f4979f4e3523e1185b05a3b9333f129ad
Dilsher10 <Dilsher.dahri10@gmail.com>
2024-02-14 03:01:31 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function displace() {
+    arrow.style.transition = '0.3s';
+    arrow.style.transform = 'translateX(20px)';
+}
+
+
+function resetDisplace() {
+    arrow.style.transition = '0.3s';
+    arrow.style.transform = 'translateX(0px)';
+}
+
+
